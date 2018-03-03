12b3d29959c4ee87fee162249614501fb728f12b
wvffle <casper@wvffle.net>
2018-03-04 00:43:46 +0100

    async parser function

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+const fs = require('fs-extra')
 
-const parse = (file, next) => {
-  return fs.readFile(file, function (err, buf) {
-    const ext = '.' + file.split('.').slice(-1)[0];
-    return next(err, buf, ext);
-  });
+const parse = async (file) => {
+  const buf = await fs.readFile(file)
+  const ext = '.' + file.split('.').slice(-1)[0];
+
+  return { content: buf, ext }
 }
 
 module.exports = server => {
